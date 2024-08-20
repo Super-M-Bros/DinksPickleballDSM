fd2fb488859632397353b306a661bb21b74b8801
Adam <adam.marks07@gmail.com>
2024-08-20 16:50:50 -0500

    responsive navbar, footer, memberships modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
-import { RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router'
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, RouterModule, RouterLinkActive],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet, RouterModule, RouterLinkActive],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'dinks_pickleball_dsm';
+    title = 'dinks_pickleball_dsm'
+    menu = false
+    toggleMenu = () => {
+        this.menu = !this.menu
+    }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
                 'dinks-blue': '#062e51',
                 'dinks-red': '#c01422',
                 'dinks-red-dark': '#760e16',
+                'dinks-green': '#225534',
                 'smoked-background': 'rgba(0, 0, 0, 0.5)',
             },
         },
