a535f7074d2b81abb1fd4b047d1e3a92a0de7ca5
mmarrano <mark.marrano@gmail.com>
2024-08-24 23:54:49 -0500

    i need to save...

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router'
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkActive, RouterModule, RouterOutlet],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  scrollToAbout() {
+    // scroll to the element named arg0
+    document.getElementById('about')?.scrollIntoView({ behavior: 'smooth' });
+  }
 
 }
